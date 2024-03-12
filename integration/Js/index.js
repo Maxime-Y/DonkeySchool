8ff248d7b92e6b15a9399b172b434ac3976f3689
maxime yehia <yehia.maxime@hotmail.fr>
2024-03-12 21:58:10 +0100

    add responsive but problem at low definition add burger menu but have to fix it

---
+++ integration/Js/index.js
@@ -0,0 +1,10 @@
+var menuButton = document.querySelector('#menu-button');
+var menu = document.querySelector('#menu');
+
+// show or hide
+menuButton.addEventListener('click',function(){
+  menu.classList.toggle('show-menu');
+  menuButton.classList.toggle('close');
+});
+
+
